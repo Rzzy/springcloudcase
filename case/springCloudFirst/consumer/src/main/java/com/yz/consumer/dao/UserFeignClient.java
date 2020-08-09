6c5a9e689dbc19a34c9cc4335046b8f2ac32d080
Rz <243566431@qq.com>
2020-08-09 23:07:38 +0800

    logging

--- case/springCloudFirst/consumer/src/main/java/com/yz/consumer/dao/UserFeignClient.java
+++ case/springCloudFirst/consumer/src/main/java/com/yz/consumer/dao/UserFeignClient.java
@@ -1,12 +1,13 @@
 package com.yz.consumer.dao;
 
+import com.yz.consumer.config.FeignConfig;
 import com.yz.consumer.dao.impl.UserFeignClientFallback;
 import com.yz.consumer.domain.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "userservice", fallback = UserFeignClientFallback.class)
+@FeignClient(value = "userservice", fallback = UserFeignClientFallback.class, configuration = FeignConfig.class)
 public interface UserFeignClient {
 
     @GetMapping("/user/{id}")
